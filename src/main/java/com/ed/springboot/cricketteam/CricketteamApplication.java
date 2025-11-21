86201bc22cb89ee2c23423273e5fb3367978f36b
srijan555 <srijanshekhar8@gmail.com>
2025-11-21 22:16:47 +0530

    eureka service discovery

--- src/main/java/com/ed/springboot/cricketteam/CricketteamApplication.java
+++ src/main/java/com/ed/springboot/cricketteam/CricketteamApplication.java
@@ -2,8 +2,10 @@ package com.ed.springboot.cricketteam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CricketteamApplication {
 
 	public static void main(String[] args) {
